527928b615f2e7393f1397a22b07622c6c03f7f0
Andrew Crawford <andrew.crawford@hireup.com.au>
2022-03-22 08:10:57 +1100

    Section 4 complete

---
+++ lottery-updated/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'indicate paddle humble toilet immense famous asset during mansion flee tunnel cash',
+    'https://rinkeby.infura.io/v3/f9dc0ac429e344b6836a2f6212543802'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+      .deploy({ data: evm.bytecode.object })
+      .send({ gas: '1000000', from: accounts[0] });
+
+  console.log(JSON.stringify(abi));
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
